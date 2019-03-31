c31c46a022a30d180d966935745857a56a9d2ab9
Marvin H. Sielenkemper <sielenk@gmx.de>
2019-03-31 14:29:47 +0200

    changed the I2C labels

--- CubeMX/Inc/main.h
+++ CubeMX/Inc/main.h
@@ -84,6 +84,10 @@ void Error_Handler(void);
 #define TX_GPIO_Port GPIOA
 #define RX_Pin GPIO_PIN_10
 #define RX_GPIO_Port GPIOA
+#define SCL_Pin GPIO_PIN_6
+#define SCL_GPIO_Port GPIOB
+#define SDA_Pin GPIO_PIN_7
+#define SDA_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
