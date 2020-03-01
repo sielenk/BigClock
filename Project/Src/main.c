084a1e1075258478ae22acc37d338cd310a7e0e5
Marvin H. Sielenkemper <sielenk@gmx.de>
2020-03-01 14:34:17 +0100

    extracted writeSegment

--- Project/Src/main.c
+++ Project/Src/main.c
@@ -80,7 +80,7 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
 	}
 }
 
-void dcf_handleTelegram(DCF* dcf) {
+void dcf_handleTelegram(DCF *dcf) {
 	if (dcf) {
 		RTC_TimeTypeDef sTime = { 0 };
 		RTC_DateTypeDef sDate = { 0 };
@@ -113,6 +113,13 @@ void dcf_handleTelegram(DCF* dcf) {
 #define SEG_H01_MASK 0xd0
 #define SEG_H10_MASK 0xe0
 
+static void writeSegment(uint8_t mask, uint8_t value) {
+	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | value;
+	GPIOA->ODR = (GPIOA->ODR & ~0xff) | mask | value;
+	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | value;
+	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK;
+}
+
 void HAL_RTCEx_RTCEventCallback(RTC_HandleTypeDef *hrtc) {
 	static int counter = 0;
 	static int test = 0;
@@ -130,25 +137,10 @@ void HAL_RTCEx_RTCEventCallback(RTC_HandleTypeDef *hrtc) {
 	const int m10 = (test ? (counter + 2) : (sTime.Minutes / 10)) % 10; // (sTime.Minutes / 10) % 10;
 	const int m01 = (test ? (counter + 3) : sTime.Minutes) % 10; // sTime.Minutes % 10;
 
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | h10;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_H10_MASK | h10;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | h10;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK;
-
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | h01;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_H01_MASK | h01;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | h01;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK;
-
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | m10;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_M10_MASK | m10;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | m10;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK;
-
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | m01;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_M01_MASK | m01;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK | m01;
-	GPIOA->ODR = (GPIOA->ODR & ~0xff) | SEG_NONE_MASK;
+	writeSegment(SEG_H10_MASK, h10);
+	writeSegment(SEG_H01_MASK, h01);
+	writeSegment(SEG_M10_MASK, m10);
+	writeSegment(SEG_M01_MASK, m01);
 }
 
 void adjustTimer(int error) {
@@ -158,41 +150,40 @@ void adjustTimer(int error) {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_RTC_Init();
-  MX_TIM3_Init();
-  MX_ADC1_Init();
-  MX_I2C1_Init();
-  MX_USB_PCD_Init();
-  MX_USART1_UART_Init();
-  MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_RTC_Init();
+	MX_TIM3_Init();
+	MX_ADC1_Init();
+	MX_I2C1_Init();
+	MX_USB_PCD_Init();
+	MX_USART1_UART_Init();
+	MX_TIM2_Init();
+	/* USER CODE BEGIN 2 */
 	HAL_GPIO_WritePin(LAMP_TEST_GPIO_Port, LAMP_TEST_Pin, GPIO_PIN_SET);
 	HAL_TIM_IC_Start(&htim3, TIM_CHANNEL_3);
 	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_4);
@@ -201,69 +192,66 @@ int main(void)
 
 	htim2.Instance->CCR4 = 0xffff;
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 		//const int x = HAL_GPIO_ReadPin(DCF_IN_GPIO_Port, DCF_IN_Pin);
 		//HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, x);
 		//ITM_SendChar('x');
 		//ITM_SendChar('\n');
 		__WFE();
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
 
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE
+			| RCC_OSCILLATORTYPE_LSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC|RCC_PERIPHCLK_ADC
-                              |RCC_PERIPHCLK_USB;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC | RCC_PERIPHCLK_ADC
+			| RCC_PERIPHCLK_USB;
+	PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
+	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -271,15 +259,14 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
