aa0dedbb8d1af2e19139fd55e2a53442cfb94d6e
Marvin H. Sielenkemper <sielenk@gmx.de>
2020-03-05 21:17:19 +0100

    auto formatted, added documentation

--- Project/Inc/dcf.hpp
+++ Project/Inc/dcf.hpp
@@ -18,31 +18,40 @@
 extern "C" {
 #endif
 
-void dcf_addBit(unsigned short pulse, unsigned short delta);
+  /* This function is called after a DCF second impulse has been finished (100 or 200ms after the second start).
+   * "pulse" is the length of the received pulse in milliseconds.
+   * "delta" is the duration from the previous to the current pulse start in milliseconds.
+   */
+  void
+  dcf_addBit(unsigned short pulse, unsigned short delta);
 
-typedef struct {
-	unsigned int :16;
-	unsigned int dstChangeComing :1;
-	unsigned int isMesz :1;
-	unsigned int isMez :1;
-	unsigned int leapSecComing :1;
-	unsigned int :1;
-	unsigned int minute01 :4;
-	unsigned int minute10 :3;
-	unsigned int :1;
-	unsigned int hour01 :4;
-	unsigned int hour10 :2;
-	unsigned int :1;
-	unsigned int day01 :4;
-	unsigned int day10 :2;
-	unsigned int weekday :3;
-	unsigned int month01 :4;
-	unsigned int month10 :1;
-	unsigned int year01 :4;
-	unsigned int year10 :4;
-}__attribute__((packed)) DCF;
+  typedef struct {
+    unsigned int :16;
+    unsigned int dstChangeComing :1;
+    unsigned int isMesz :1;
+    unsigned int isMez :1;
+    unsigned int leapSecComing :1;
+    unsigned int :1;
+    unsigned int minute01 :4;
+    unsigned int minute10 :3;
+    unsigned int :1;
+    unsigned int hour01 :4;
+    unsigned int hour10 :2;
+    unsigned int :1;
+    unsigned int day01 :4;
+    unsigned int day10 :2;
+    unsigned int weekday :3;
+    unsigned int month01 :4;
+    unsigned int month10 :1;
+    unsigned int year01 :4;
+    unsigned int year10 :4;
+  } __attribute__((packed)) DCF;
 
-void dcf_handleTelegram(DCF const* dcf);
+  /* Is called after the first pulse of a minute is over (100ms after the minute start).
+   * The argument is not null if the previous telegram has been successfully decoded.
+   */
+  void
+  dcf_handleTelegram(DCF const *dcf);
 
 #ifdef __cplusplus
 }
